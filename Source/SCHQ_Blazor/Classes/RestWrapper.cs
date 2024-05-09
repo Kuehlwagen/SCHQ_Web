0ddf2b3167f3f7c6305980b1a657d957c6588532
Manuel <kuehlwagen@outlook.com>
2024-05-09 22:50:02 +0200

    REST POST Relations

---
+++ Source/SCHQ_Blazor/Classes/RestWrapper.cs
@@ -0,0 +1,8 @@
+﻿using SCHQ_Protos;
+
+namespace SCHQ_Blazor.Classes;
+public class RestSetRelationsRequest {
+  public string? Channel { get; set; }
+  public string? Password { get; set; }
+  public List<RelationInfo>? Relations { get; set; }
+}
--- Source/SCHQ_Blazor/Program.cs
+++ Source/SCHQ_Blazor/Program.cs
@@ -1,4 +1,8 @@
+using Grpc.Net.Client;
+using SCHQ_Blazor.Classes;
 using SCHQ_Blazor.Components;
+using SCHQ_Protos;
+using static SCHQ_Protos.SCHQ_Relations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -39,5 +43,37 @@ app.MapControllers();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode();
+
+app.MapPost("/relations", async (RestSetRelationsRequest request) => {
+  IResult result = Results.Empty;
+  try {
+    string? gRPC_Url = builder.Configuration.GetValue<string>("gRPC_Url");
+    if (gRPC_Url != null) {
+      SCHQ_RelationsClient gRPC_Client = new(GrpcChannel.ForAddress(gRPC_Url,
+        new() {
+          HttpHandler = new SocketsHttpHandler() {
+            PooledConnectionIdleTimeout = Timeout.InfiniteTimeSpan,
+            KeepAlivePingDelay = TimeSpan.FromSeconds(60),
+            KeepAlivePingTimeout = TimeSpan.FromSeconds(30),
+            EnableMultipleHttp2Connections = true
+          }
+        }));
+      var rpc_request = new SetRelationsRequest() {
+        Channel = request.Channel,
+        Password = request.Password
+      };
+      rpc_request.Relations.AddRange(request.Relations!);
+      SuccessReply rtnVal = await gRPC_Client.SetRelationsAsync(rpc_request);
+      if (rtnVal.Success) {
+        result = Results.Ok(rtnVal);
+      } else {
+        result = Results.BadRequest(rtnVal);
+      }
+    }
+  } catch (Exception ex) {
+    result = Results.Conflict(ex);
+  }
+  return result;
+});
 
 app.Run();
